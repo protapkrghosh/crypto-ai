e77373db0be861006f4dd8e779a2926897332af1
Protap Ghosh <protapg518@gmail.com>
2023-10-29 14:57:24 +0600

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="text-2xl">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
