4d9bdd41409f41fcf66f3b9e7af8e9c7964e27d6
Protap Ghosh <protapg518@gmail.com>
2023-10-31 02:28:15 +0600

    added input field in footer section

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <footer className="flex-none md:flex py-16">
           <div>
             <img src={logo} alt="" className='w-28' />
-            <p className='text-[#64677C] mt-10 md:w-[70%] mb-10 md:mb-0'>ChatAI, make life full of AI, talk to AI in the web page, in the telegram, in all chat tools, and AI answers any answer.</p>
+            <p className='text-[#64677C] text-[15px] mt-10 md:w-[70%] mb-10 md:mb-0'>ChatAI, make life full of AI, talk to AI in the web page, in the telegram, in all chat tools, and AI answers any answer.</p>
           </div>
 
           <div className='footer'>
@@ -26,9 +26,9 @@ const Footer = () => {
             </nav>
             <nav>
               <header className="text-xl text-[#DDD7E9]">Contact Us</header>
-              <a className="link link-hover">Terms of use</a>
-              <a className="link link-hover">Privacy policy</a>
-              <a className="link link-hover">Cookie policy</a>
+              <div className="mt-5">
+                <input type="text" placeholder="Enter Your Email" className="text-[14px] text-white placeholder:text-white input input-bordered bg-[#7A49FF] w-60 pr-16 rounded-full pl-6" />
+              </div>
             </nav>
           </div>
         </footer>
