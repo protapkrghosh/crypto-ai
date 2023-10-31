98effd10ab461d8e853722573815219405a049ad
Protap Ghosh <protapg518@gmail.com>
2023-10-31 23:33:29 +0600

    title responsiveness

--- src/pages/Home/Roadmap/Roadmap.jsx
+++ src/pages/Home/Roadmap/Roadmap.jsx
@@ -8,20 +8,20 @@ const Roadmap = () => {
   return (
     <div className='bg-gradient-to-b from-[#130e33] from-5% via-[#030014] via-50% to-[#1b1542] to-100% -mt-[1px] pt-12 pb-20'>
       <Container>
-        <div className='flex justify-between items-center'>
+        <div className='flexMap'>
           <div>
             <h4 className='text-4xl md:text-6xl text-white'>Our <span className='TextShadow'>Roadmap</span></h4>
           </div>
 
           {/* chat board icon */}
-          <div className='flex items-center gap-10'>
+          <div className='flex items-center gap-10 mt-3 md:mt-0'>
             <div className='flex items-center'>
-              <img src={ChatIcon} alt="Chat Icon" className='w-7' />
-              <p className='text-[#64677C] ml-3'>Chatbubble</p>
+              <img src={ChatIcon} alt="Chat Icon" className='w-5 md:w-6' />
+              <p className='text-[15px] text-[#64677C] ml-3'>Chatbubble</p>
             </div>
             <div className='flex items-center'>
-              <img src={LayersSharp} alt="Layers Sharp" className='w-9' />
-              <p className='text-[#64677C] ml-3'>layers-sharp</p>
+              <img src={LayersSharp} alt="Layers Sharp" className='w-6 md:w-8' />
+              <p className='text-[15px] text-[#64677C] ml-3'>layers-sharp</p>
             </div>
           </div>
         </div>
